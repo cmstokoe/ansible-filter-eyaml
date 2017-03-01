527c32d45b3867b8433898ad075fa460607a8bd4
Jan Scheufler <jan.scheufler@t-systems.com>
2017-03-01 19:11:36 +0100

    made eyaml.py look for a configuration file
    configuration file is expected to be in the same directory as the script
    path to private and public key is configurable

--- eyaml.py
+++ eyaml.py
@@ -1,13 +1,23 @@
+# usage
+## The in-line encrypted variable can be referenced and automaticly de-crypted within your ansible plays/roles using the following Jinja2 instantiation {{ secret_variable | eyaml }}.
 import os
 import subprocess
 import sys
+import yaml
 
 def eyaml(arg):
-    FNULL = open(os.devnull, 'w')
-    cmd="eyaml decrypt -s \"" + arg + "\""
-    output = subprocess.check_output(cmd, stderr=FNULL, shell=True)
-    return output.rstrip();
+  path_to_script = os.path.dirname(os.path.realpath(__file__))
+  with open(path_to_script + "/eyaml.yml", 'r') as ymlfile:
+    cfg = yaml.load(ymlfile)
+
+    eyaml_private_key=cfg['eyaml_private_key']
+    eyaml_public_key=cfg['eyaml_public_key']
+
+  FNULL = open(os.devnull, 'w')
+  cmd="eyaml decrypt --pkcs7-private-key=" + eyaml_private_key + " --pkcs7-public-key=" + eyaml_public_key + " -s \"" + arg + "\""
+  output = subprocess.check_output(cmd, stderr=FNULL, shell=True)
+  return output.rstrip();
 
 class FilterModule(object):
-     def filters(self):
-         return {'eyaml': eyaml}
+   def filters(self):
+       return {'eyaml': eyaml}
